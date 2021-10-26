8df28c577c4e956f45eba49e5c1abbe35c1df63f
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-10-26 10:27:35 +0300

    Lint/format C++ version

--- C++/src/bruteforcer.cpp
+++ C++/src/bruteforcer.cpp
@@ -1,59 +1,70 @@
 #include <string>
 using namespace std;
 
-class Bruteforcer {
-    public:
-        Bruteforcer(string password_to_crack, int min, int max) {
-            password = password_to_crack;
-            min_length = min;
-            max_length = max;
-            guesses = 1;
-        }
-        string get_alphanumeric() {
-            return "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-        }
-        int guess_length() {
-            int guess_length = rand() % (max_length + 1);
+class Bruteforcer
+{
+public:
+    Bruteforcer(string password_to_crack, int min, int max)
+    {
+        password = password_to_crack;
+        min_length = min;
+        max_length = max;
+        guesses = 1;
+    }
+    string get_alphanumeric()
+    {
+        return "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+    }
+    int guess_length()
+    {
+        int guess_length = rand() % (max_length + 1);
 
-            if( guess_length < min_length ) {
-                return min_length;
+        if (guess_length < min_length)
+        {
+            return min_length;
+        }
+
+        return guess_length;
+    }
+    char get_random_char()
+    {
+        string alphanumeric = get_alphanumeric();
+        int random_number = rand() % (alphanumeric.length() - 1);
+
+        return alphanumeric[random_number];
+    }
+    long int break_password()
+    {
+        bool is_not_cracked = true;
+
+        while (is_not_cracked)
+        {
+            string current_guess = "";
+            int length = guess_length();
+
+            for (int i = 0; i < length; i++)
+            {
+                current_guess.push_back(get_random_char());
             }
 
-            return guess_length;
-        }
-        char get_random_char() {
-            string alphanumeric = get_alphanumeric();
-            int random_number = rand() % (alphanumeric.length() - 1);
+            printf("%ld guess is: %s \n", guesses, current_guess.c_str());
 
-            return alphanumeric[random_number];
-        }
-        long int break_password() {
-            bool is_not_cracked = true;
-
-            while(is_not_cracked) {
-                string current_guess = "";
-                int length = guess_length();
-
-                for(int i = 0; i < length; i++){
-                    current_guess.push_back(get_random_char());
-                }
-
-                printf("%ld guess is: %s \n", guesses, current_guess.c_str());
-
-                if(password.compare(current_guess) == 0) {
-                    printf("Found a match!! \n");
-                    printf("%s \n", current_guess.c_str());
-                    is_not_cracked = false;
-                }
-
-                guesses++;
+            if (password.compare(current_guess) == 0)
+            {
+                printf("Found a match!! \n");
+                printf("%s \n", current_guess.c_str());
+                is_not_cracked = false;
             }
 
-            return guesses;
+            guesses++;
         }
-    private:
-        string password;
-        unsigned short int min_length;
-        unsigned short int max_length;
-        long int guesses;        
+
+        return guesses;
+    }
+
+private:
+    string password;
+    unsigned short int min_length;
+    unsigned short int max_length;
+    long int guesses;
 };
