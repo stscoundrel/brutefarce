fb3b6a497956c64ebc4b2fd820e34b629c1c4914
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-10-26 10:27:35 +0300

    Lint/format C++ version

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -2,11 +2,12 @@
 #include "bruteforcer.cpp"
 using namespace std;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     string password = argv[1];
-    unsigned short int min_length = atoi(argv[2]); 
+    unsigned short int min_length = atoi(argv[2]);
     unsigned short int max_length = atoi(argv[3]);
-    
+
     Bruteforcer farce(password, min_length, max_length);
 
     long result = farce.break_password();
