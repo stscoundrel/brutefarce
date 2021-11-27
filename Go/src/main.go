c37ae8c201221edbc3b466aec10c8a1171774ed1
Sampo Silvennoinen <20028934+stscoundrel@users.noreply.github.com>
2021-11-27 09:21:49 +0200

    Feature/go (#8)
    Add Go implementation

---
+++ Go/src/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"brutefarce/bruteforce"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) < 3 {
+		fmt.Println("Too few arguments provided. Provide password, min length and max length")
+		os.Exit(1)
+	}
+
+	minLength, minErr := strconv.Atoi(args[0])
+	maxLength, maxErr := strconv.Atoi(args[1])
+	password := args[2]
+
+	if minErr != nil || maxErr != nil {
+		fmt.Println("Could not parse min & max length. Check you're using numbers")
+		os.Exit(1)
+	}
+
+	totalGuesses := bruteforce.BreakPassword(minLength, maxLength, password)
+
+	fmt.Println(fmt.Sprintf("Cracked the password in %d guesses!", totalGuesses))
+}
