aad63f6c7e654528a3cb2df6885bada56d805bc6
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-11-27 09:15:58 +0200

    Go: add bruteforce package implementation

--- Go/src/bruteforce/bruteforce.go
+++ Go/src/bruteforce/bruteforce.go
@@ -1,5 +1,42 @@
 package bruteforce
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func getGuessLength(minLength int, maxLength int) int {
+	return rand.Intn(maxLength+1-minLength) + minLength
+}
+
+func getNewGuess(guessLength int) string {
+	guess := make([]byte, guessLength)
+	for i := range guess {
+		guess[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(guess)
+}
+
 func BreakPassword(minLength int, maxLength int, password string) int {
-	return 12
+	rand.Seed(time.Now().UnixNano())
+	isNotCracked := true
+	guesses := 1
+
+	for isNotCracked {
+		guessLength := getGuessLength(minLength, maxLength)
+		guess := getNewGuess(guessLength)
+		fmt.Println(guesses, ". guess is ", guess)
+
+		if guess == password {
+			fmt.Println("Found a match! ", guess)
+			isNotCracked = false
+		}
+
+		guesses += 1
+	}
+
+	return guesses
 }
