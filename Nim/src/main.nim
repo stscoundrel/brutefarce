2e62f626fa79805af0264d31590ff4c5e8f5b2ec
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-11-27 09:26:51 +0200

    Nim: fix order of cmd line args

--- Nim/src/main.nim
+++ Nim/src/main.nim
@@ -7,9 +7,9 @@ if paramCount() < 3:
     quit(QuitFailure)
 
 let
-  minLength: int = parseInt(paramStr(1))
-  maxLength: int = parseInt(paramStr(2))
-  password: string = paramStr(3)
+  password: string = paramStr(1)
+  minLength: int = parseInt(paramStr(2))
+  maxLength: int = parseInt(paramStr(3))
 
 let totalGuesses: int =  bruteforce(minLength, maxLength, password)
 
