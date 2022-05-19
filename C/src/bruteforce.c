9671d941489f7e458d9c07efec2b36c5dabea905
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-05-19 19:24:32 +0300

    C: Add header file

--- C/src/bruteforce.c
+++ C/src/bruteforce.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "bruteforce.h"
 
 long bruteforce_randomly(char password[], int min_length, int max_length)
 {
---
+++ C/src/bruteforce.h
@@ -0,0 +1,6 @@
+#ifndef BRUTEFORCE_H
+#define BRUTEFORCE_H
+
+long bruteforce_randomly(char password[], int min_length, int max_length);
+
+#endif /* BRUTEFORCE_H */
