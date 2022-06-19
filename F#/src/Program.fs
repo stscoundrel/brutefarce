f351b0f993005f800e0c3aaad1dcdd0d06214298
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-06-19 09:43:51 +0300

    Add F# scaffold

---
+++ F#/src/Program.fs
@@ -0,0 +1,15 @@
+﻿[<EntryPoint>]
+let main args =
+    
+    if args.Length < 3 then
+       printfn "Too few arguments provided. Provide password, min length and max length"
+
+    let password :string = args[0]
+    let minLength: byte = args[1] |> byte
+    let maxLength: byte = args[2] |> byte
+
+    printfn "%s" password
+    printfn "%d" minLength
+    printfn "%d" maxLength
+
+    0
