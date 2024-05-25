9287079697d0d68263d30e4678ab9899fe96df3a
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-05-25 17:44:03 +0300

    C: optimize guess alloc
    Previously independently allocated each guess in loop. Now just allocates one memory block to be used for all guesses to be made.
    
    Closes #17

--- C/src/bruteforce.c
+++ C/src/bruteforce.c
@@ -9,27 +9,25 @@ long bruteforce_randomly(char password[], int min_length, int max_length)
 {
     srand(time(NULL));
     const char ALPHANUMERIC[62] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+    int alphanumeric_length = sizeof(ALPHANUMERIC) - 1;
     bool is_not_cracked = true;
     unsigned long guesses = 1;
 
+    // Allocate max length variable to be reused.
+    char *current_guess = malloc(sizeof(char) * (max_length + 1));
+
     while (is_not_cracked)
     {
-        int length = rand() % max_length;
+        int length = rand() % (max_length - min_length + 1) + min_length;
 
-        if (length < min_length)
+        for (int i = 0; i < length; i++)
         {
-            length = min_length;
-        }
-
-        char *current_guess = malloc(sizeof(char) * (length + 1));
-
-        int i;
-        for (i = 0; i < length; i++)
-        {
-            int random_number = rand() % (sizeof(ALPHANUMERIC) - 1);
+            int random_number = rand() % alphanumeric_length;
             char random_char = ALPHANUMERIC[random_number];
             current_guess[i] = random_char;
         }
+        // Terminate leftovers from previous guesses.
+        current_guess[length] = '\0'; 
 
         printf("%lu guess is: %s \n", guesses, current_guess);
 
@@ -41,10 +39,10 @@ long bruteforce_randomly(char password[], int min_length, int max_length)
         }
 
         guesses++;
-
-        // https://www.youtube.com/watch?v=Fgb8l2Ve8vo
-        free(current_guess);
     }
+
+    // https://www.youtube.com/watch?v=Fgb8l2Ve8vo
+    free(current_guess);
 
     return guesses;
 }
